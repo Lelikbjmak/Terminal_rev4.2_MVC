490ee1fdcfbe7e4fcf3c2f6b462d72ffcf51aaa6
Denis Solovey <denissolovey990@gmail.com>
2022-08-12 16:05:46 +0300

    add site

---
+++ src/main/java/com/example/Terminal_rev42/Config/SpringConfig.java
@@ -0,0 +1,72 @@
+package com.example.Terminal_rev42.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+@ComponentScan("com.example.Terminal_rev42")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext appl;
+
+    @Autowired
+    public SpringConfig(ApplicationContext applicationContext){
+        this.appl = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setApplicationContext(appl);
+        springResourceTemplateResolver.setPrefix("/WEB-INF/views/Terminal/");
+        springResourceTemplateResolver.setSuffix(".html");
+        springResourceTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        springResourceTemplateResolver.setCacheable(true);
+        return springResourceTemplateResolver;
+    }
+
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry resolverRegistry){
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        resolverRegistry.viewResolver(resolver);
+    }
+
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("").addResourceLocations("webapp/WEB-INF/views/Terminal/css/");
+////        registry.addResourceHandler("/*.png").addResourceLocations("/WEB-INF/views/Terminal/logo");
+//////        registry.addResourceHandler("/").addResourceLocations("/WEB-INF/views/Terminal/pics");
+////        registry.addResourceHandler("/*.html").addResourceLocations("/WEB-INF/views/Terminal");
+//    }
+
+//    @Override
+//    public void configureDefaultServletHandling(
+//            DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
+
+}
