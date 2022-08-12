0801d88830cf51141dff632281910571c2902c18
Denis Solovey <denissolovey990@gmail.com>
2022-08-12 21:30:06 +0300

    deploy site

---
+++ src/main/java/com/example/Terminal_rev42/Config/Configuration.java
@@ -0,0 +1,21 @@
+package com.example.Terminal_rev42.Config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Configuration extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
