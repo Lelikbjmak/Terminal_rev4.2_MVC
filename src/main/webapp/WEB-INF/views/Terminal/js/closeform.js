e8223dd89cc830517468cd7fcad975b199667d5d
Denis Solovey <denissolovey990@gmail.com>
2022-08-15 19:42:40 +0300

    add animation with JS to operation forms eradicate some drawbacks

---
+++ src/main/webapp/WEB-INF/views/Terminal/js/closeform.js
@@ -0,0 +1,14 @@
+
+var wins = document.querySelectorAll(".form-pop");
+
+var ov = document.getElementById('ov');
+
+ov.addEventListener('click', function(){
+for(var i = 0; i < wins.length; i++){
+wins[i].style.opacity = '0%';
+wins[i].style.zIndex = '3';
+}
+ov.style.opacity = '0%';
+ov.style.zIndex = '0';
+});
+
