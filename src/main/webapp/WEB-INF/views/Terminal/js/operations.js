8c2cbf8bbe2224321ed165c371427050a5f0f896
Denis Solovey <denissolovey990@gmail.com>
2022-08-22 20:56:54 +0300

    js animation for operations

---
+++ src/main/webapp/WEB-INF/views/Terminal/js/operations.js
@@ -0,0 +1,50 @@
+function openconvert(){
+var ov = document.getElementById('ov');
+const win = document.getElementById('convert');
+
+ov.style.opacity = '100%';
+ov.style.zIndex = '8';
+
+win.style.opacity = "100%";
+win.style.zIndex = '10';
+}
+
+
+function opencashtransfer(){
+var ov = document.getElementById('ov');
+const win = document.getElementById('cashtransfer');
+
+ov.style.opacity = '100%';
+ov.style.zIndex = '8';
+
+win.style.opacity = "100%";
+win.style.zIndex = '10';
+}
+
+
+function opendeposit(){
+var ov = document.getElementById('ov');
+const win = document.getElementById('deposit');
+
+ov.style.opacity = '100%';
+ov.style.zIndex = '8';
+
+win.style.opacity = "100%";
+win.style.zIndex = '10';
+}
+
+
+function opencashextradition(){
+var ov = document.getElementById('ov');
+const win = document.getElementById('cashextradition');
+
+ov.style.opacity = '100%';
+ov.style.zIndex = '8';
+
+win.style.opacity = "100%";
+win.style.zIndex = '10';
+}
+
+
+
+
