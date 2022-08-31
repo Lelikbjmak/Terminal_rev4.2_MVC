934e102927b479c1602ada098d4cc2e18e7a720e
Denis Solovey <denissolovey990@gmail.com>
2022-08-31 09:38:18 +0300

    jQuery Post deposit

--- src/main/webapp/WEB-INF/views/Terminal/js/depositPOST.js
+++ src/main/webapp/WEB-INF/views/Terminal/js/depositPOST.js
@@ -5,6 +5,9 @@ $(document).ready(
 
                event.preventDefault();
 
+                $('.wrap').css({'opacity':'100%', 'z-index':'12'});
+
+
                 var billfrom = $("#billfrom2").val();
                 var currency = $("#currency2").val();
                 var summa = $("#summa2").val();
@@ -16,25 +19,40 @@ $(document).ready(
                     summa: summa,
 
                 }, function(data) {
-                    var json = JSON.parse(data);
-                }).done(function(){
-                }).fail(function(xhr, textStatus, errorThrown) {
-                }).complete(function(data){
-                     $form.each(function(){
-                          this.reset();  // или так очищается форма методом .reset()
-                        });
-                   var wins = document.querySelectorAll(".form-pop");
 
-                   var ov = document.getElementById('ov');
-
-                   for(var i = 0; i < wins.length; i++){
-                   wins[i].style.opacity = '0%';
-                   wins[i].style.zIndex = '0';
-                   }
-                   ov.style.opacity = '0%';
-                   ov.style.zIndex = '0';
+                      }).done(function(data, textStatus, jqXHR){
+                        setTimeout(() => {
+                     $('div.message').text(jqXHR.responseText);
+                     $('.loader').css({'opacity':'0%', 'z-index':'0'});
+                     $('.bl').css({'opacity':'100%', 'z-index':'12'})
+                     }, 3000);
 
 
+                }).fail(function(jqXHR, exception, textStatus, errorThrown) {
+                    var msg = '';
+                    if (jqXHR.status === 0) {
+                        msg = 'Not connect. Verify Network.';
+                    } else if (jqXHR.status == 404) {
+                        msg = 'Requested page not found. [404]';
+                    } else if (jqXHR.status == 500) {
+                        msg = 'Internal Server Error [500].';
+                    }else if (jqXHR.status == 400) {
+                        msg = jqXHR.responseText;
+                    }else if (exception === 'parsererror') {
+                        msg = 'Requested JSON parse failed.';
+                    } else if (exception === 'timeout') {
+                        msg = 'Time out error.';
+                    } else if (exception === 'abort') {
+                        msg = 'Ajax request aborted.';
+                    } else {
+                        msg = 'Uncaught Error. ' + jqXHR.responseText;
+                    }
+
+                    setTimeout(() => {
+                    $('div.message').text(msg);
+                    $('.loader').css('opacity','0%');
+                    $('.bl').css({'opacity':'100%', 'z-index':'12'})
+                    }, 3000);
                 });
 
         });
