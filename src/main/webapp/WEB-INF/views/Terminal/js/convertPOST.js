ad5c20dda15c2a0e277370069493935226b9ac76
Denis Solovey <denissolovey990@gmail.com>
2022-08-31 09:38:26 +0300

    jQuery Post convert

--- src/main/webapp/WEB-INF/views/Terminal/js/convertPOST.js
+++ src/main/webapp/WEB-INF/views/Terminal/js/convertPOST.js
@@ -5,6 +5,9 @@ $(document).ready(
 
                 event.preventDefault();
 
+                $('.wrap').css({'opacity':'100%', 'z-index':'12'});
+
+
                 var billfrom = $("#billfrom3").val();
                 var currency = $("#currency3").val();
                 var summa = $("#summa3").val();
@@ -18,12 +21,39 @@ $(document).ready(
                     summa: summa,
                     pin: pin
                 }, function(data) {
-                    alert(data);
-                }).done(function(data, textStatus, jqXHR){
-                    $("#ov").css({'opacity':'0%', 'z-index':'0'});
-                    $(".form-pop").css({'opacity':'0%', 'z-index':'0'});
-                    $(".con").trigger('reset');
-                }).fail(function(xhr, textStatus, errorThrown) {
+
+                 }).done(function(data, textStatus, jqXHR){
+                        setTimeout(() => {
+                     $('div.message').text(jqXHR.responseText);
+                     $('.loader').css({'opacity':'0%', 'z-index':'0'});
+                     $('.bl').css({'opacity':'100%', 'z-index':'12'})
+                     }, 3000);
+
+                }).fail(function(jqXHR, exception, textStatus, errorThrown) {
+                    var msg = '';
+                    if (jqXHR.status === 0) {
+                        msg = 'Not connect. Verify Network.';
+                    } else if (jqXHR.status == 404) {
+                        msg = 'Requested page not found. [404]';
+                    } else if (jqXHR.status == 500) {
+                        msg = 'Internal Server Error [500].';
+                    }else if (jqXHR.status == 400) {
+                        msg = jqXHR.responseText;
+                    }else if (exception === 'parsererror') {
+                        msg = 'Requested JSON parse failed.';
+                    } else if (exception === 'timeout') {
+                        msg = 'Time out error.';
+                    } else if (exception === 'abort') {
+                        msg = 'Ajax request aborted.';
+                    } else {
+                        msg = 'Uncaught Error. ' + jqXHR.responseText;
+                    }
+
+                    setTimeout(() => {
+                    $('div.message').text(msg);
+                    $('.loader').css('opacity','0%');
+                    $('.bl').css({'opacity':'100%', 'z-index':'12'})
+                    }, 3000);
                 });
 
 
