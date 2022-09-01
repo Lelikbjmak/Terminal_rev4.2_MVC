53b1a29ac9100858bad1860c05ac7247484b01aa
Denis Solovey <denissolovey990@gmail.com>
2022-09-01 18:35:19 +0300

    Add register validation final rev

--- src/main/webapp/WEB-INF/views/Terminal/js/regvalid.js
+++ src/main/webapp/WEB-INF/views/Terminal/js/regvalid.js
@@ -4,6 +4,8 @@ const confirmpassword = document.getElementById("confirmpassword");
 
 const form = document.getElementById("go");
 
+const forma = document.getElementById("regg");  // our form
+
 var win = document.getElementById('client');
 
 var ov = document.getElementById('ovvv');
@@ -12,6 +14,7 @@ var userwin = document.getElementById('user');
 
 
 function check(){
+
     let flag = true;
     // get values from inputs
     var usernamevalue = username.value.trim();
@@ -20,16 +23,19 @@ function check(){
 
 
     if(usernamevalue === ""){
-    // show error
-    flag = false;
-    setErrorFor(username, "Username can't be blank");
+        // show error
+        flag = false;
+        setErrorFor(username, "Username can't be blank");
+    }else if(usernamevalue.length < 4){
+        flag = false;
+        setErrorFor(username, "Username must contain at least 4 symbols")
     }else{
-    var url = "/Barclays/client/checkUsername?username=" + usernamevalue;
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, false);
-    xhr.send();
+        var url = "/Barclays/client/checkUsername?username=" + usernamevalue;
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, false);
+        xhr.send();
 
-    if(xhr.response === "false"){
+        if(xhr.response === "false"){
             setErrorFor(username, "Username already exist");
             flag = false;
         }else{
@@ -39,22 +45,29 @@ function check(){
 
 
     if(passwordvalue === ""){
-     // show error
-     flag = false;
-     setErrorFor(password, "Username can't be blank");
-     }else{
+        // show error
+        flag = false;
+        setErrorFor(password, "Password can't be blank");
+    }else if(passwordvalue.length < 8){
+              flag = false;
+              setErrorFor(password, "Password must contain at least 8 symbols");
+    }else{
         setSuccessFor(password);
-     }
+    }
 
     if(confirmpasswordvalue === ""){
-     setErrorFor(confirmpassword, "Confirm password");
-     flag = false;
-     }else if(confirmpasswordvalue != passwordvalue){
-     setErrorFor(confirmpassword, "Password doesn't match");
-     flag = false;
-     }else{
+        setErrorFor(confirmpassword, "Confirm password");
+        flag = false;
+    }else if(confirmpasswordvalue != passwordvalue) {
+        setErrorFor(confirmpassword, "Password doesn't match");
+        flag = false;
+    }else if(confirmpasswordvalue.length < 8){
+         flag = false;
+         setErrorFor(confirmpassword, "Password must contain at least 8 symbols");
+    }
+    else{
         setSuccessFor(confirmpassword);
-     }
+    }
 
      return flag;  // if true -> 0 mistakes -> open client form
 }
@@ -85,14 +98,6 @@ win.style.zIndex = '10';
 }
 
 
-function checkusername(username){
-var url = "/Barclays/client/checkUsername?username=" + username;
-fetch(url).then(function(response){
-    response.text().then(function(text){
-    alert("text:" + text);
-  });
-});
-}
 
 form.addEventListener('click', (e) =>{
 
@@ -100,24 +105,87 @@ form.addEventListener('click', (e) =>{
         openclientregform();
     }
 
-    var reg = /^([a-zа-яё]+[\s]{0,1}[a-zа-яё]+[\s]{0,1}[a-zа-яё])$/ig;
-     // ig any substring in any register
-    var str = "Иван Иванов";
-    var str_en = "Petya Petrovich Pupkin";
-    var str_not_work = "Petya Petrovich Pupkin sfsdfsdf";
-
-    console.log(reg.test(str));
-    console.log(reg.test(str_en));
-    console.log(reg.test(str_not_working));
-
 });
 
-const forma = document.getElementById("regg");
 
-regg.addEventListener('submit', (e) => {
+
+
+forma.addEventListener('submit', (e) => {
     e.preventDefault();
-    check1();
+    if(check1()){
+
+
+$(document).ready(
+    function($) {
+
+        $('.wrap').css({'opacity':'100%', 'z-index':'12'});
+
+
+        var username = $("#username").val().trim();
+        var password = $("#password").val().trim();
+        var confirmedpassword = $("#confirmpassword").val().trim();
+        var name = $("#name").val().trim();
+        var passport = $("#passport").val().trim();
+        var birth = $("#bday").val();
+        var phone = $("#phone").val().trim();
+
+
+        $.post("/Barclays/client/add", {
+            username: username,  //1st in Java | 2nd here
+            password: password,
+            confirmedpassword: confirmedpassword,
+            name: name,
+            passport: passport,
+            birth: birth,
+            phone: phone
+        }, function(data) {
+
+         }).done(function(data, textStatus, jqXHR){
+                setTimeout(() => {
+                    $('div.message').text(jqXHR.responseText);
+                    $('.loader').css({'opacity':'0%', 'z-index':'0'});
+                }, 2500);
+
+               setTimeout(function() {
+                    document.location.href = "/Barclays";
+                    $('div.message').text('');
+                    $('.wrap').css({'opacity':'0%', 'z-index':'0'});
+                    $('.loader').css('opacity','100%');
+                    $("#regg").trigger('reset');
+               }, 5000);
+
+        }).fail(function(jqXHR, exception, errorThrown) {
+            var msg = '';
+            if (jqXHR.status === 0) {
+                msg = 'Not connect. Verify Network.';
+            } else if (jqXHR.status == 404) {
+                msg = 'Requested page not found. [404]';
+            } else if (jqXHR.status == 500) {
+                msg = 'Internal Server Error [500].';
+            }else if (jqXHR.status == 400) {
+                msg = jqXHR.responseText;
+            }else if (exception === 'parsererror') {
+                msg = 'Requested JSON parse failed.';
+            } else if (exception === 'timeout') {
+                msg = 'Time out error.';
+            } else if (exception === 'abort') {
+                msg = 'Ajax request aborted.';
+            } else {
+                msg = 'Uncaught Error. ' + jqXHR.responseText;
+            }
+            alert(msg);
+
+        });
+
+
+
+
+    });
+
+
+    }
 });
+
 
 const name = document.getElementById("name");
 const passport = document.getElementById("passport");
@@ -126,14 +194,51 @@ const phone = document.getElementById("phone");
 
 
 function check1(){
+
+var flag = true;
 const namevalue = name.value.trim();
 const passportvalue = passport.value.trim();
-const birth = birth.value;
+const birthvalue = birth.value;
 const phonevalue = phone.value.trim();
 
+var namereg = /^([A-Z]{1}[a-z]*\s?){2}|([А-Я]{1}[а-я]*\s?){2}$/;
+var passportreg = /^[A-Z]{2}\d{7}$/;
+
 if(namevalue === ''){
-setErrorFor(name, "Name can't be blank")
+    setErrorFor(name, "Name can't be blank");
+    flag = false;
+}else if(!namereg.test(namevalue)){
+    setErrorFor(name, "Not valid format");
+    flag = false;
+}else{
+    setSuccessFor(name);
 }
 
+
+if(passportvalue === ''){
+    setErrorFor(passport, "Passport can't be blank")
+    flag = false;
+}else if(!passportreg.test(passportvalue)){
+    setErrorFor(passport, "Not valid format");
+    flag = false;
+}else{
+    setSuccessFor(passport);
+}
+
+if(birthvalue === ''){
+    setErrorFor(birth, "Birth can't be blank")
+    flag = false;
+}else{
+    setSuccessFor(birth);
+}
+
+if(phonevalue === ''){
+    setErrorFor(phone, "Phone can't be blank")
+    flag = false;
+}else{
+    setSuccessFor(phone);
+}
+
+return flag;
 
 }
