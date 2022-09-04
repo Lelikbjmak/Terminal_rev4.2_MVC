006c4066496b81c7ea277aa5f1bf7a42c5671361
Denis Solovey <denissolovey990@gmail.com>
2022-09-04 23:18:56 +0300

    add closing of card extradition form

--- src/main/webapp/WEB-INF/views/Terminal/js/closecardreg.js
+++ src/main/webapp/WEB-INF/views/Terminal/js/closecardreg.js
@@ -1,14 +1,137 @@
 
-var ov = document.getElementById('ovvv');
+var ov = document.getElementById('ov');
+
+var ww = document.getElementsByClassName('form-pop');
+
+var formm = document.querySelectorAll('.con');
 
 var win = document.getElementById('cardreg');
 
+var wrap = document.getElementById('wrap');
+
+var but = document.getElementById('prev');
+
+var buts = document.getElementsByClassName('dws-menu');
+
+var header = document.querySelectorAll('.menuaut1');
+
+var wrap = document.getElementById('wrap');
+
 ov.addEventListener('click', function(){
 
-win.style.opacity = '0%';
-win.style.zIndex = '2';
+
+const rr = document.getElementsByClassName('r2 success');
+
+const rrr = document.getElementsByClassName('r2 error');
+
+for (let r of rrr) {
+      r.classList.remove('error');
+    }
+
+for (let r of rrr) {
+      r.classList.remove('error');
+    }
+
+for (let r of rr) {
+      r.classList.remove('success');
+    }
+
+for (let r of rr) {
+      r.classList.remove('success');
+    }
 
 ov.style.opacity = '0%';
 ov.style.zIndex = '0';
+
+win.style.opacity = '0%';
+win.style.zIndex = '0';
+
+for(var i = 0; i < ww.length; i++){
+ww[i].style.opacity = '0%';
+ww[i].style.zIndex = '0';
+}
+
+for(var i = 0; i< buts.length; i++){
+buts[i].style.zIndex = '3';
+}
+
+for(var i = 0; i < header.length; i++){
+header[i].style.zIndex = '3';
+}
+
+for(var i = 0; i < formm.length; i++){
+formm[i].reset();
+}
+
 });
+
+
+wrap.addEventListener('click', function(){
+
+const rr = document.getElementsByClassName('r2 success');
+
+const rrr = document.getElementsByClassName('r2 error');
+
+for (let r of rrr) {
+      r.classList.remove('error');
+    }
+
+for (let r of rrr) {
+      r.classList.remove('error');
+    }
+
+for (let r of rr) {
+      r.classList.remove('success');
+    }
+
+for (let r of rr) {
+      r.classList.remove('success');
+    }
+
+
+ov.style.opacity = '0%';
+ov.style.zIndex = '0';
+
+
+wrap.style.opacity = '0%';
+wrap.style.zIndex = '0';
+
+
+var msg = document.getElementById('message');
+msg.innerHTML = "";
+
+
+var ccc = document.querySelectorAll('.bl');
+for(var i = 0; i< ccc.length; i++){
+ccc[i].style.opacity = '0%';
+}
+
+
+var loader = document.querySelectorAll('.loader');
+for(var i = 0; i< loader.length; i++){
+loader[i].style.opacity = '100%';
+}
+
+for(var i = 0; i < ww.length; i++){
+ww[i].style.opacity = '0%';
+ww[i].style.zIndex = '0';
+}
+
+for(var i = 0; i< buts.length; i++){
+buts[i].style.zIndex = '3';
+}
+
+for(var i = 0; i < header.length; i++){
+header[i].style.zIndex = '3';
+}
+
+for(var i = 0; i < formm.length; i++){
+formm[i].reset();
+}
+
+});
+
+
+
+
 
