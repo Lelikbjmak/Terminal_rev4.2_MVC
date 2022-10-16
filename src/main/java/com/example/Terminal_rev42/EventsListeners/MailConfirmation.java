1017b81aa386ef164edffd6a202185b7fcea612a
Denis Solovey <denissolovey990@gmail.com>
2022-10-16 22:22:41 +0300

    Add mail confirmation event to send confirmation email in order to verify account

---
+++ src/main/java/com/example/Terminal_rev42/EventsListeners/MailConfirmation.java
@@ -0,0 +1,36 @@
+package com.example.Terminal_rev42.EventsListeners;
+
+import com.example.Terminal_rev42.Model.user;
+import org.springframework.context.ApplicationEvent;
+
+public class MailConfirmation extends ApplicationEvent {
+
+    private String appUrl;
+    private com.example.Terminal_rev42.Model.user user;
+
+    public MailConfirmation(user user, String appUrl) {
+
+        super(user);
+
+        this.user = user;
+
+        this.appUrl = appUrl;
+
+    }
+
+    public String getAppUrl() {
+        return appUrl;
+    }
+
+    public void setAppUrl(String appUrl) {
+        this.appUrl = appUrl;
+    }
+
+    public com.example.Terminal_rev42.Model.user getUser() {
+        return user;
+    }
+
+    public void setUser(com.example.Terminal_rev42.Model.user user) {
+        this.user = user;
+    }
+}
