af3acc8b4d834e41d97d57f8b202788208d1e9bc
Denis Solovey <denissolovey990@gmail.com>
2022-10-16 22:21:01 +0300

    Take into account that user must have enabled account to authenticate

--- src/main/java/com/example/Terminal_rev42/SeviceImplementation/UserDetailedServiceImpl.java
+++ src/main/java/com/example/Terminal_rev42/SeviceImplementation/UserDetailedServiceImpl.java
@@ -4,8 +4,10 @@ import com.example.Terminal_rev42.Model.Role;
 import com.example.Terminal_rev42.Model.user;
 import com.example.Terminal_rev42.Repositories.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,23 +23,44 @@ public class UserDetailedServiceImpl implements UserDetailsService  {
     @Autowired
     private UserDAO userDAO;
 
-
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println("loadUserByUsername activated...");
+
+        System.err.println("loadUserByUsername activated...");
         System.out.println(username);
+
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+
         user user = userDAO.findByUsername(username);
-        System.out.println("password: " + user.getPassword());
-        if (user == null) throw new UsernameNotFoundException(username);
+
+        if (user == null)
+            throw new UsernameNotFoundException(username);
+
+
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
         for (Role role : user.getRoleset()){
             grantedAuthorities.add(new SimpleGrantedAuthority(role.getRole()));
         }
 
-        System.out.println(grantedAuthorities.toString());
+        System.out.println(grantedAuthorities);
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+//        User springuser = new User(
+//                user.getUsername(),
+//                user.getPassword(),
+//                user.isEnabled(),
+//                accountNonExpired,
+//                credentialsNonExpired,
+//                accountNonLocked,
+//                grantedAuthorities
+//        );
+
+        UserDetails user1 = User.withUsername(user.getUsername()).password(user.getPassword()).disabled(!user.isEnabled()).accountExpired(false).credentialsExpired(false).accountLocked(false).authorities(grantedAuthorities).build();
+
+        return user1;
     }
 
 }
