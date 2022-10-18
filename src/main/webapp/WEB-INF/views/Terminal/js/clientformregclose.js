f113569966a44a92085d928fd9efcd4c22f365db
Denis Solovey <denissolovey990@gmail.com>
2022-10-18 23:14:25 +0300

    close all forms of registration + refuse auto redirect

--- src/main/webapp/WEB-INF/views/Terminal/js/clientformregclose.js
+++ src/main/webapp/WEB-INF/views/Terminal/js/clientformregclose.js
@@ -5,6 +5,9 @@ var ov = document.getElementById('ovvv');
 
 var userwin = document.getElementById('user');
 
+const backButton = document.getElementById('prev');
+
+
 ov.addEventListener('click', function(){
 
 userwin.style.opacity = '100%';
@@ -16,3 +19,43 @@ ov.style.opacity = '0%';
 ov.style.zIndex = '0';
 });
 
+
+backButton.addEventListener('click', function(){
+
+const rr = document.getElementsByClassName('r2 success');
+
+const rrr = document.getElementsByClassName('r2 error');
+
+for (let r of rrr) {
+      r.classList.remove('error');
+    }
+
+for (let r of rrr) {
+      r.classList.remove('error');
+    }
+
+for (let r of rr) {
+      r.classList.remove('success');
+    }
+
+for (let r of rr) {
+      r.classList.remove('success');
+    }
+
+
+userwin.style.opacity = '100%';
+
+win.style.opacity = '0%';
+win.style.zIndex = '2';
+
+ov.style.opacity = '0%';
+ov.style.zIndex = '0';
+
+$('div.message').text('');
+$('div.message1.small').text('');
+
+$('.wrap').css({'opacity':'0%', 'z-index':'0'});
+$('.loader').css('opacity','100%');
+$("#regg").trigger('reset');
+
+});
