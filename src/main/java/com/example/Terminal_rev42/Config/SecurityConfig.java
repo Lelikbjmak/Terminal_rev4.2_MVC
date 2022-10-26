68fd8438ec6571c6ff55eaacc910a5d3bd563e72
Denis Solovey <denissolovey990@gmail.com>
2022-10-26 22:47:00 +0300

    Rebuild deprecated WebSecurityConfigurerAdapter and add Session management

--- src/main/java/com/example/Terminal_rev42/Config/SecurityConfig.java
+++ src/main/java/com/example/Terminal_rev42/Config/SecurityConfig.java
@@ -1,72 +1,107 @@
 package com.example.Terminal_rev42.Config;
 
+import com.example.Terminal_rev42.SeviceImplementation.UserDetailsPasswordServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import java.security.SecureRandom;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig{
 
     @Qualifier("UserDetailsServiceImpl")  // our ovverride userdetilsSerice (implementing of UDS)
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Qualifier("UserDetailsPasswordServiceImpl")
+    @Autowired
+    private UserDetailsPasswordServiceImpl userDetailsPasswordService;
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(16, new SecureRandom());
     }
 
 
-//    @Override
-//    public void configure(WebSecurity web) {
-//        web.ignoring()
-//                .antMatchers("/Terminal/**");
-//    }
-
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf()
                 .disable()
                 .authorizeRequests()
-                //permit any identity to visit that pages
-                //.antMatchers("/Barclays", "/Barclays/reg", "/Barclays/client/add").not().fullyAuthenticated()
-                .antMatchers("/","/Barclays", "/Barclays/reg", "/Barclays/client/**", "/Terminal/**", "/Barclays/bill/PercentageForFixed", "/Barclays/bad", "/Barclays/success" ).permitAll()
-                .antMatchers("/Barclays/service/**").hasAnyRole("USER", "ADMIN")
-                //.antMatchers("/", "/Terminal/css", "/Terminal/") for static resources
-                .anyRequest().authenticated()
-                .and()
+//                .antMatchers(HttpMethod.DELETE)
+//                .hasRole("ADMIN")
 
+//                .antMatchers("/Barclays/admin/**")
+//                .hasAnyRole("ADMIN")
+
+                .antMatchers("/Barclays/bill/**", "/Barclays/service/**")
+                .hasAnyRole("USER", "ADMIN")
+
+                .antMatchers("/Barclays", "/Barclays/reg", "/Barclays/client/**",  "/Barclays/bad", "/Barclays/success", "/Barclays/authorisation")
+                .permitAll()
+
+                .antMatchers("/Terminal/**")
+                .permitAll()
+
+                .anyRequest()
+                .authenticated()
+
+                .and()
                 .formLogin()
                 .loginPage("/Barclays/authorisation")
                 .defaultSuccessUrl("/Barclays")
+                .failureUrl("/Barclays/authorisation?message=Invalid%20Username%20or%20Password!")
                 .permitAll()
+
+                .and()
+                .rememberMe()
+
                 .and()
                 .logout()
-                .permitAll()
-                .logoutSuccessUrl("/Barclays");
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessUrl("/Barclays")
+                //.tokenValiditySeconds()   2 weaks - default
 
-        }
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .sessionFixation().migrateSession()
+                .maximumSessions(1)
+                .expiredUrl("/Barclays/bad?ms=Session%20expire");
 
 
+
+
+        return http.build();
+    }
     @Bean
-    public AuthenticationManager customAuthenticationManager() throws Exception{
-        return authenticationManager();
+    public AuthenticationManager customAuthenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception{
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .userDetailsPasswordManager(userDetailsPasswordService)
+                .and()
+                .build();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+    @Bean  // to control sessions
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
     }
 
 }
