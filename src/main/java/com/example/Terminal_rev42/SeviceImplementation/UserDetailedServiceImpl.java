2439b2bc2bf1e405b6ec74b7c0ff1934818e479a
Denis Solovey <denissolovey990@gmail.com>
2022-10-29 17:08:50 +0300

    add logging

--- src/main/java/com/example/Terminal_rev42/SeviceImplementation/UserDetailedServiceImpl.java
+++ src/main/java/com/example/Terminal_rev42/SeviceImplementation/UserDetailedServiceImpl.java
@@ -3,8 +3,9 @@ package com.example.Terminal_rev42.SeviceImplementation;
 import com.example.Terminal_rev42.Model.Role;
 import com.example.Terminal_rev42.Model.user;
 import com.example.Terminal_rev42.Repositories.UserDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -23,12 +24,11 @@ public class UserDetailedServiceImpl implements UserDetailsService  {
     @Autowired
     private UserDAO userDAO;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserDetailedServiceImpl.class);
+
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        System.err.println("loadUserByUsername activated...");
-        System.out.println(username);
 
         boolean accountNonExpired = true;
         boolean credentialsNonExpired = true;
@@ -46,8 +46,6 @@ public class UserDetailedServiceImpl implements UserDetailsService  {
             grantedAuthorities.add(new SimpleGrantedAuthority(role.getRole()));
         }
 
-        System.out.println(grantedAuthorities);
-
 //        User springuser = new User(
 //                user.getUsername(),
 //                user.getPassword(),
@@ -58,9 +56,13 @@ public class UserDetailedServiceImpl implements UserDetailsService  {
 //                grantedAuthorities
 //        );
 
+
         UserDetails user1 = User.withUsername(user.getUsername()).password(user.getPassword()).disabled(!user.isEnabled()).accountExpired(false).credentialsExpired(false).accountLocked(false).authorities(grantedAuthorities).build();
+
+        logger.info("User: " + username + " is logging...");
 
         return user1;
     }
+
 
 }
