163a8493dad9702bf2f231e29baf55994a8d9b54
Denis Solovey <denissolovey990@gmail.com>
2022-10-30 22:49:41 +0300

    refactor closing form with card download link

--- src/main/webapp/WEB-INF/views/Terminal/js/closecardreg.js
+++ src/main/webapp/WEB-INF/views/Terminal/js/closecardreg.js
@@ -71,76 +71,6 @@ formm[i].reset();
 });
 
 
-wrap.addEventListener('click', function(){
-
-const rr = document.getElementsByClassName('r2 success');
-
-const rrr = document.getElementsByClassName('r2 error');
-
-for (let r of rrr) {
-      r.classList.remove('error');
-    }
-
-for (let r of rrr) {
-      r.classList.remove('error');
-    }
-
-for (let r of rr) {
-      r.classList.remove('success');
-    }
-
-for (let r of rr) {
-      r.classList.remove('success');
-    }
-
-
-ov.style.opacity = '0%';
-ov.style.zIndex = '0';
-
-win.style.opacity = '0%';
-win.style.zIndex = '0';
-
-wrap.style.opacity = '0%';
-wrap.style.zIndex = '0';
-
-
-var msg = document.getElementById('message');
-msg.innerHTML = "";
-
-
-var ccc = document.querySelectorAll('.bl');
-for(var i = 0; i< ccc.length; i++){
-ccc[i].style.opacity = '0%';
-}
-
-
-var loader = document.querySelectorAll('.loader');
-for(var i = 0; i< loader.length; i++){
-loader[i].style.opacity = '100%';
-}
-
-for(var i = 0; i < ww.length; i++){
-ww[i].style.opacity = '0%';
-ww[i].style.zIndex = '0';
-}
-
-rrt.style.opacity = '0%';
-rrt.style.zIndex = '0';
-
-
-for(var i = 0; i< buts.length; i++){
-buts[i].style.zIndex = '3';
-}
-
-for(var i = 0; i < header.length; i++){
-header[i].style.zIndex = '3';
-}
-
-for(var i = 0; i < formm.length; i++){
-formm[i].reset();
-}
-
-});
 
 
 but.addEventListener('click', function(){
@@ -216,3 +146,22 @@ formm[i].reset();
 });
 
 
+var recbutton = document.getElementById("rec");
+
+//alert(recbutton.parentElement);
+
+
+
+recbutton.addEventListener("click", function(){
+    alert("rebuild card# " + datafromcardreg);
+    savecardfile();
+    but.removeAttribute("disabled");
+});
+
+
+function savecardfile(){
+    let link = document.createElement("a");
+    link.setAttribute("href", "http://localhost:8080/Barclays/bill/card?card=" + datafromcardreg);
+    link.setAttribute("download", datafromcardreg);
+    link.click();
+}
