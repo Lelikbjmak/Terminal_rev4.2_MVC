428982376a3441cec125fc0ef738fa212ccaa421
Denis Solovey <denissolovey990@gmail.com>
2022-11-17 23:32:57 +0300

    Add bill validation methods include(Exceptions)

--- src/main/java/com/example/Terminal_rev42/Servicies/billService.java
+++ src/main/java/com/example/Terminal_rev42/Servicies/billService.java
@@ -1,30 +1,47 @@
 package com.example.Terminal_rev42.Servicies;
 
 import com.example.Terminal_rev42.Entities.bill;
+import com.example.Terminal_rev42.Exceptions.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Collection;
 import java.util.Set;
 
 public interface billService {
 
-    void addbill(bill bill);
+    void save(bill bill);
 
     bill findByCard(String card);
 
     Set<bill> AllBillsByClientId(long id);
 
-    bill getRegBill(Collection<String> bills);
-
     Set<bill> inActiveBills(LocalDate date);
 
-    void diactivateBill(bill bill);
+    void deactivateBill(bill bill);
 
     Set<bill> notifyBillsByValidityLessThan(int days);
 
-    boolean checkpin(bill bill, String pin);
+    boolean checkPin(bill bill, String pin);
 
-    void encodePassAndActivate(bill bill);
+    void encodePasswordAndActivateBill(bill bill);
 
-    bill lastcard(Set<String> bills, long id);
+    void unlockCard(bill bill);
+
+    Set<bill> allLatelyInteractedBills(long id, int attempts);
+
+    void resetFailedAttempts(bill bill);
+
+    void increaseFailedAttempts(bill bill);
+
+    void lockBill(bill bill);
+
+    boolean checkLedger(bill bill, BigDecimal summa) throws NotEnoughLedgerException;
+
+    boolean checkCurrencyEquals(String currency, bill bill);
+
+    bill fullBillValidationBeforeOperation(String card) throws BillNotFoundException, BillInactiveException, TemporaryLockedBillException;
+
+    boolean pinAndLedgerValidation(bill bill, String rawPassword, BigDecimal summa) throws IncorrectBillPinException, NotEnoughLedgerException;
+
+    boolean pinValidation(bill bill, String rawPassword) throws IncorrectBillPinException;
 }
