8977c21eb6d7056f55228d7e50c6a869625959ae
Denis Solovey <denissolovey990@gmail.com>
2022-11-17 23:33:18 +0300

    Implement bill validation methods from billService include(Exceptions)

--- src/main/java/com/example/Terminal_rev42/SeviceImplementation/billServiceImpl.java
+++ src/main/java/com/example/Terminal_rev42/SeviceImplementation/billServiceImpl.java
@@ -1,14 +1,18 @@
 package com.example.Terminal_rev42.SeviceImplementation;
 
 import com.example.Terminal_rev42.Entities.bill;
+import com.example.Terminal_rev42.Exceptions.*;
 import com.example.Terminal_rev42.Repositories.BillRepository;
 import com.example.Terminal_rev42.Servicies.billService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Collection;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 @Service
@@ -20,8 +24,10 @@ public class billServiceImpl implements billService {
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
 
+    private static final Logger logger = LoggerFactory.getLogger(billServiceImpl.class);
+
     @Override
-    public void addbill(bill bill) {
+    public void save(bill bill) {
         billRepository.save(bill);
     }
 
@@ -35,10 +41,6 @@ public class billServiceImpl implements billService {
         return billRepository.findByClient_idAndActiveIsTrue(id);
     }
 
-    @Override
-    public bill getRegBill(Collection<String> bills) {
-        return billRepository.findFirstByCardInOrderByValidityDesc(bills);
-    }
 
     @Override
     public Set<bill> inActiveBills(LocalDate date) {
@@ -46,7 +48,8 @@ public class billServiceImpl implements billService {
     }
 
     @Override
-    public void diactivateBill(bill bill) {
+    public void deactivateBill(bill bill) {
+
         bill.setActive(false);
         billRepository.save(bill);
     }
@@ -57,23 +60,129 @@ public class billServiceImpl implements billService {
     }
 
     @Override
-    public boolean checkpin(bill bill, String pin) {
-        if(passwordEncoder.matches(pin, bill.getPin()))
-            return true;
-        else return false;
+    public boolean checkPin(bill bill, String pin) {
+
+        return passwordEncoder.matches(pin, bill.getPin());
+
     }
 
     @Override
-    public void encodePassAndActivate(bill bill) {
-        System.out.println("bill: " + bill.getCard() + ", pin: " + bill.getPin());
+    public void encodePasswordAndActivateBill(bill bill) {
         bill.setPin(passwordEncoder.encode(bill.getPin()));
         bill.setActive(true);
+        logger.info("Bill " + bill.getCard() + " is activated.");
         billRepository.save(bill);
     }
 
+
     @Override
-    public bill lastcard(Set<String> bills, long id) {
-        return billRepository.findByCardNotInAndClient_idAndActiveIsTrue(bills, id);
+    public void unlockCard(bill bill) {
+
+        if (bill.isTemporalLock()) {
+
+            if (LocalDateTime.now().isAfter(bill.getLockTime().plusDays(com.example.Terminal_rev42.Entities.bill.LOCK_TIME_DURATION))) {
+                bill.setTemporalLock(false);
+                bill.setFailedAttempts(0);
+                bill.setLockTime(null);
+                logger.info("Bill " + bill.getCard() + " is unlocked after temporary block.");
+                billRepository.save(bill);
+            }
+        }
+
+    }
+
+    @Override
+    public Set<bill> allLatelyInteractedBills(long id, int attempts) {
+        return billRepository.findByClient_idAndActiveIsTrueAndTemporalLockIsFalseAndFailedAttemptsGreaterThan(id, attempts);
+    }
+
+    @Override
+    public void resetFailedAttempts(bill bill) {
+
+        bill.setFailedAttempts(0);
+        billRepository.save(bill);
+
+    }
+
+    @Override
+    public void increaseFailedAttempts(bill bill) {
+
+        bill.setFailedAttempts(bill.getFailedAttempts() + 1);
+
+        if(bill.getFailedAttempts() == 3){
+            logger.warn("Bill " + bill.getCard() + " is locked due to 3 failed attempts.");
+            lockBill(bill);
+        }
+
+        billRepository.save(bill);
+
+    }
+
+    @Override
+    public void lockBill(bill bill) {
+
+        bill.setLockTime(LocalDateTime.now());
+        bill.setTemporalLock(true);
+        billRepository.save(bill);
+
+    }
+
+    @Override
+    public boolean checkLedger(bill bill, BigDecimal summa) {
+        return bill.getLedger().compareTo(summa) >=0 ? true : false;
+    }
+
+
+    @Override
+    public boolean checkCurrencyEquals(String currency, bill bill) {
+        return bill.getCurrency().equals(currency);
+    }
+
+
+    @Override
+    public bill fullBillValidationBeforeOperation(String card) throws BillNotFoundException, BillInactiveException, TemporaryLockedBillException {
+
+        bill bill = findByCard(card);
+
+        if(bill == null)
+            throw new BillNotFoundException("Bill " + card + " doesn't exist.", card);
+
+        if(!bill.isActive())
+            throw new BillInactiveException("Bill " + card + " is out of validity. Expired date: " + bill.getValidity(), bill);
+
+        unlockCard(bill); // if bill is active, but there is a likelihood that it may be temporary locked -> try to unlock due to interaction with bill from client
+
+        if(bill.isTemporalLock())
+            throw new TemporaryLockedBillException("Bill " + card + " was temporary locked due to 3 failed attempts. It will be unlocked " + bill.getLockTime().plusDays(1), bill);
+
+        return bill;
+
+    }
+
+
+    @Override
+    public boolean pinAndLedgerValidation(bill bill, String rawPassword, BigDecimal summa) throws IncorrectBillPinException, NotEnoughLedgerException {
+
+        if(!checkPin(bill, rawPassword)){
+            increaseFailedAttempts(bill);
+            throw new IncorrectBillPinException("Incorrect pin. Attempts left: " + Integer.toString(com.example.Terminal_rev42.Entities.bill.MAX_FAILED_ATTEMPTS - bill.getFailedAttempts()) + ".", bill);
+        }
+
+        if (!checkLedger(bill, summa))
+            throw new NotEnoughLedgerException("Not enough ledger to pay " + summa, bill);
+
+        return true;
+    }
+
+    @Override
+    public boolean pinValidation(bill bill, String rawPassword) throws IncorrectBillPinException {
+
+        if(!checkPin(bill, rawPassword)){
+            increaseFailedAttempts(bill);
+            throw new IncorrectBillPinException("Incorrect pin. Attempts left: " + Integer.toString(com.example.Terminal_rev42.Entities.bill.MAX_FAILED_ATTEMPTS - bill.getFailedAttempts()) + ".", bill);
+        }
+
+        return true;
     }
 
 
