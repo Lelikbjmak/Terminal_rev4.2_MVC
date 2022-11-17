45fc5e036a6217ca1e798b4c3f40016113882d57
Denis Solovey <denissolovey990@gmail.com>
2022-11-17 23:27:15 +0300

    Not enough ledger exception

---
+++ src/main/java/com/example/Terminal_rev42/Exceptions/NotEnoughLedgerException.java
@@ -0,0 +1,22 @@
+package com.example.Terminal_rev42.Exceptions;
+
+import com.example.Terminal_rev42.Entities.bill;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class NotEnoughLedgerException extends Exception{
+
+    private final bill bill;
+
+    public NotEnoughLedgerException(String message, bill bill){
+        super(message);
+        this.bill = bill;
+
+    }
+
+    public bill getBill() {
+        return bill;
+    }
+
+}
--- src/main/java/com/example/Terminal_rev42/Servicies/receiptsService.java
+++ src/main/java/com/example/Terminal_rev42/Servicies/receiptsService.java
@@ -4,14 +4,11 @@ import com.example.Terminal_rev42.Entities.bill;
 import com.example.Terminal_rev42.Entities.receipts;
 
 import java.util.Collection;
-import java.util.Optional;
 
 public interface receiptsService {
 
     void save(receipts receipt);
 
-    Optional<receipts> findById(long id);
-
-    receipts findFirstByBillfromInOrderByIdDesc(Collection<bill> bills);
+    receipts findFirstByBillInOrderByIdDesc(Collection<bill> bills);
 
 }
