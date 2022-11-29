2dd353fae6d9196ed4065e720fd1d72197a6043f
Denis Solovey <denissolovey990@gmail.com>
2022-11-29 18:08:22 +0300

    use properties from application-test.properties

--- src/test/resources/create-user-before-register.sql
+++ src/test/resources/create-user-before-register.sql
@@ -4,11 +4,11 @@ delete from ver_token;
 delete from users;
 
 insert into users values
-(1, 1, 0, null , 'test@mail.ru', '$2a$16$KQ3G.w0tbnCwx6t4ubAoVezFnGJeTaetbWTzHkYB.2IM6HkgaL5N6', null, 0,  'test');
+(1, 1, 0, null , 'testUser@gmail.com', '$2a$16$KQ3G.w0tbnCwx6t4ubAoVezFnGJeTaetbWTzHkYB.2IM6HkgaL5N6', 'testResetPasswordToken', 0,  'testUser');
 
-insert into ver_token values (1, null , now() + interval 1 day, 'testToken', 1);
+insert into ver_token values (1, null , now() + interval 1 day, 'testVerificationToken', 1);
 
 insert into users_roles values
 (1, 1);
 
-insert into client values(1, '2000-02-02', 'Test Test', 'AB1234567', '1234567', 1);
+insert into client values(1, '2002-02-02', 'First Last Middle', 'AB1111111', '111111111', 1);
