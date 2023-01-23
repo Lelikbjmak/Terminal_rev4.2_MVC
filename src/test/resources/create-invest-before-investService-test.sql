30b1542f3079417a3cb0a7c953b7a5be8dffe851
Denis Solovey <denissolovey990@gmail.com>
2023-01-23 21:23:25 +0300

    .sql for InvestServiceImpl Tests

---
+++ src/test/resources/create-invest-before-investService-test.sql
@@ -0,0 +1,20 @@
+delete from investments;
+delete from client;
+delete from users_roles;
+delete from users;
+
+insert into users values
+(1, 1, 0, null , 'testUser@gmail.com', '$2a$16$KQ3G.w0tbnCwx6t4ubAoVezFnGJeTaetbWTzHkYB.2IM6HkgaL5N6', null, 0,  'testUser'),
+(2, 0, 0, null, 'test2@mail.ru', '$2a$16$KQ3G.w0tbnCwx6t4ubAoVezFnGJeTaetbWTzHkYB.2IM6HkgaL5N6',null, 0, 'test2'),
+(3, 1, 3, '2025-11-09 22:10:36.895625', 'test3@mail.ru', '$2a$16$8G6RReUHW8rma7mBLbLIduTBKeouL3w68Y85oMB/RFAkGw0HB6XTm', null, 1, 'test3');
+
+insert into users_roles values
+(1, 1),
+(2, 1),
+(3, 1);
+
+insert into client values(1, '2002-02-02', 'First Last Middle', 'AB1111111', '111111111', 1),
+                         (2, '2002-02-02', 'First Last Middle', 'AB2222222', '111111111', 2);
+
+insert into investments values(1, '2023-01-01', 100.00, 'USD', 10.00, 1, 12, 'Test type', 1),
+                              (2, '2022-01-01', 100.00, 'USD', 10.00, 0, 12, 'Test type', 1);
---
+++ src/test/resources/drop-investments-after-investService-tests.sql
@@ -0,0 +1,4 @@
+delete from investments;
+delete from client;
+delete from users_roles;
+delete  from users;
