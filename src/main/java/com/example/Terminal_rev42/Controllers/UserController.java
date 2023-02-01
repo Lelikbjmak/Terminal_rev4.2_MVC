871a45c1e01634fac8a9ab88dbcfbe553cf0e316
Lelikbjmak <denissolovey990@gmail.com>
2023-02-01 19:57:17 +0300

    add change password mapping

--- src/main/java/com/example/Terminal_rev42/Controllers/UserController.java
+++ src/main/java/com/example/Terminal_rev42/Controllers/UserController.java
@@ -2,9 +2,12 @@ package com.example.Terminal_rev42.Controllers;
 
 import com.example.Terminal_rev42.Exceptions.IncorrectPasswordException;
 import com.example.Terminal_rev42.Exceptions.PasswordAndConfirmedPasswordNotMatchException;
+import com.example.Terminal_rev42.Model.User;
+import com.example.Terminal_rev42.SeviceImplementation.UserDetailsPasswordServiceImpl;
 import com.example.Terminal_rev42.SeviceImplementation.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,38 +20,48 @@ public class UserController {
     @Autowired
     private UserServiceImpl userService;
 
-    @PostMapping("/checkPassword")
+    @Autowired
+    private UserDetailsPasswordServiceImpl userDetailsPasswordService;
+
+    @PostMapping("checkPassword")
     @ResponseBody
     public Map<String, Boolean> checkPasswordFor(@RequestBody Map<String, String> passwords,
                                                  @SessionAttribute("SPRING_SECURITY_CONTEXT")SecurityContext securityContext)
             throws IncorrectPasswordException {
 
-        String currentPassword = passwords.get("password");
+        String currentPassword = passwords.get("oldPassword");
         String username = securityContext.getAuthentication().getName();
 
-        System.err.println(username);
 
         if (userService.passwordMatch(currentPassword, username))
             return Map.of("match", true);
-
         else throw new IncorrectPasswordException("Incorrect password.", currentPassword,
                 securityContext.getAuthentication());
     }
     @PostMapping("changePassword")
     @ResponseBody
     public Map<String, String> changePassword(@RequestBody Map<String, String> passwords,
-                                              @SessionAttribute("SPRING_SECURITY_CONTEXT") SecurityContext securityContext) throws PasswordAndConfirmedPasswordNotMatchException {
+                                              @SessionAttribute("SPRING_SECURITY_CONTEXT") SecurityContext securityContext) throws PasswordAndConfirmedPasswordNotMatchException, IncorrectPasswordException {
 
         String newPassword = passwords.get("newPassword");
         String confirmedNewPassword = passwords.get("confirmedNewPassword");
+        UserDetails user = (UserDetails) securityContext.getAuthentication().getPrincipal();
 
-        if (validationBeforePasswordChanging(newPassword, confirmedNewPassword))
+        if (validationBeforePasswordChanging(newPassword, confirmedNewPassword, securityContext)) {
+            userDetailsPasswordService.updatePassword(user, newPassword);
             return Map.of("message", "Password successfully changed!");
+        }
 
         return passwords;
     }
 
-    private boolean validationBeforePasswordChanging(String newPass, String confirmedPass) throws PasswordAndConfirmedPasswordNotMatchException {
+    private boolean validationBeforePasswordChanging(String newPass, String confirmedPass, SecurityContext context) throws PasswordAndConfirmedPasswordNotMatchException, IncorrectPasswordException {
+
+        if (newPass.isBlank())
+            throw new IncorrectPasswordException("Password is mandatory.", newPass, context.getAuthentication());
+        else if (newPass.length() < 8)
+            throw new IncorrectPasswordException("Must contain at least 8 symbols.", newPass, context.getAuthentication());
+
         if(newPass.equals(confirmedPass))
             return true;
         else throw new PasswordAndConfirmedPasswordNotMatchException("Passwords don't match.", newPass,
