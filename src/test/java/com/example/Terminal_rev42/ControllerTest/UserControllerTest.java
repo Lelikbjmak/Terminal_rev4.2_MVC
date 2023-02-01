9119bd57fc535ecf786c1ad592752628ee7729d9
Lelikbjmak <denissolovey990@gmail.com>
2023-02-01 19:57:44 +0300

    test for User controller (change + check password)

--- src/test/java/com/example/Terminal_rev42/ControllerTest/UserControllerTest.java
+++ src/test/java/com/example/Terminal_rev42/ControllerTest/UserControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.Terminal_rev42.ControllerTest;
 
-import org.junit.jupiter.api.MethodOrderer;
+import com.example.Terminal_rev42.Model.User;
+import com.example.Terminal_rev42.SeviceImplementation.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -27,19 +29,55 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private UserServiceImpl userService;
+
     @Test
     @Sql(value = {"/create-user-before-register.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(value = {"/drop-user-after-registration.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @WithUserDetails(value = "testUser")
-    public void successfullyChangePassword() throws Exception {
+    public void successfullyChangePassword(@Value("${client.add.password.value.to}") String currentPassword,
+                                           @Value("${client.add.username.value}") String username) throws Exception {
+        String newPassword = "22222222";
+        String confirmedNewPassword = "22222222";
 
-        String passwords = "{\"oldPassword\" : \"1111\", \"newPassword\" : \"2222\", \"confirmedNewPassword\" : \"2222\"}";
+        User user = userService.findByUsername(username);
+
+        String passwords = "{\"oldPassword\" : \"" + currentPassword + "\", \"newPassword\" : \"" + newPassword + "\"," +
+                "\"confirmedNewPassword\" : \"" + confirmedNewPassword + "\"}";
+
+        Assertions.assertTrue(userService.passwordMatch(currentPassword, username));
 
         mockMvc.perform(post("/Barclays/user/changePassword")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(passwords))
                 .andDo(print())
                 .andExpect(status().isOk());
+
+        Assertions.assertTrue(userService.passwordMatch(newPassword, username));
+
+    }
+
+    @Test
+    @Sql(value = {"/create-user-before-register.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = {"/drop-user-after-registration.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @WithUserDetails(value = "testUser")
+    public void failedChangePasswordNotValidFormat(@Value("${client.add.password.value.to}") String currentPassword) throws Exception {
+
+        String notValidLength = "{\"oldPassword\" : \"" + currentPassword + "\", \"newPassword\" : \"2222\", \"confirmedNewPassword\" : \"2222\"}";
+        String blankPass = "{\"oldPassword\" : \"" + currentPassword + "\", \"newPassword\" : \"2222\", \"confirmedNewPassword\" : \"2222\"}";
+
+        mockMvc.perform(post("/Barclays/user/changePassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(notValidLength))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(post("/Barclays/user/changePassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(blankPass))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
 
@@ -47,14 +85,29 @@ public class UserControllerTest {
     @Sql(value = {"/create-user-before-register.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
     @Sql(value = {"/drop-user-after-registration.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     @WithUserDetails(value = "testUser")
-    public void successfullyCheckPassword() throws Exception {
+    public void successfullyCheckPassword(@Value("${client.add.password.value.to}") String currentPassword) throws Exception {
 
-        String passwords = "{\"oldPassword\" : \"1111\", \"newPassword\" : \"2222\", \"confirmedNewPassword\" : \"2222\"}";
+        String passwords = "{\"oldPassword\" : \"" + currentPassword + "\"}";
 
         mockMvc.perform(post("/Barclays/user/checkPassword")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(passwords))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @Sql(value = {"/create-user-before-register.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = {"/drop-user-after-registration.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @WithUserDetails(value = "testUser")
+    public void failedCheckPassword() throws Exception {
+
+        String passwords = "{\"oldPassword\" : \"incorrectPassword\"";
+
+        mockMvc.perform(post("/Barclays/user/checkPassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(passwords))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 }
