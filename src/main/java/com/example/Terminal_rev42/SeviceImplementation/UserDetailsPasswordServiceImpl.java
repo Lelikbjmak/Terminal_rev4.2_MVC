6ed604d70168e6b3eba57a8dd34a0af0d6a2bbe3
Lelikbjmak <denissolovey990@gmail.com>
2023-02-01 23:50:26 +0300

    refactor method to update password

--- src/main/java/com/example/Terminal_rev42/SeviceImplementation/UserDetailsPasswordServiceImpl.java
+++ src/main/java/com/example/Terminal_rev42/SeviceImplementation/UserDetailsPasswordServiceImpl.java
@@ -1,45 +1,35 @@
 package com.example.Terminal_rev42.SeviceImplementation;
 
-import com.example.Terminal_rev42.Model.Role;
 import com.example.Terminal_rev42.Model.User;
 import com.example.Terminal_rev42.Repositories.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Service("UserDetailsPasswordServiceImpl")
 @Transactional(propagation = Propagation.REQUIRED)
 public class UserDetailsPasswordServiceImpl implements UserDetailsPasswordService {
 
+    @Autowired
+    private UserDAO userDAO;
 
     @Autowired
-    private UserDAO userDAO;;
+    private UserDetailedServiceImpl userDetailedService;
 
     @Override
     public UserDetails updatePassword(UserDetails user, String newPassword) {
 
-        System.err.println("Update password activated... " + user.getUsername() + ", new pass: " + newPassword);
-        User user1 = userDAO.findByUsername(user.getUsername());
-        System.err.println("prev pass: " + user1.getPassword());
+        System.err.println("UserDetailsPasswordService: ");
 
-        user1.setPassword(newPassword);
+        User user1 = userDAO.findByUsername(user.getUsername());
+        user1.setPassword(new BCryptPasswordEncoder().encode(newPassword));
         userDAO.save(user1);
 
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-
-        for (Role role : user1.getRoleset()){
-            grantedAuthorities.add(new SimpleGrantedAuthority(role.getRole()));
-        }
-
-      return org.springframework.security.core.userdetails.User.withUsername(user1.getUsername()).password(user1.getPassword()).disabled(!user1.isEnabled()).accountExpired(false).credentialsExpired(false).accountLocked(false).authorities(grantedAuthorities).build();
+        return userDetailedService.loadUserByUsername(user1.getUsername());
     }
 
 
