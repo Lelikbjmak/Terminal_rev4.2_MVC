9639fa3cfda619d5df05a0999f090ea5bd4c29b5
Lelikbjmak <denissolovey990@gmail.com>
2023-02-03 20:15:34 +0300

    Add exceptionHandlers to InvalidUsername/UsernameAlreadyExist via changing username

--- src/main/java/com/example/Terminal_rev42/ControllerAdvicers/UserControllerAdviser.java
+++ src/main/java/com/example/Terminal_rev42/ControllerAdvicers/UserControllerAdviser.java
@@ -1,8 +1,11 @@
 package com.example.Terminal_rev42.ControllerAdvicers;
 
 import com.example.Terminal_rev42.Controllers.BillController;
+import com.example.Terminal_rev42.Controllers.UserController;
 import com.example.Terminal_rev42.Exceptions.IncorrectPasswordException;
+import com.example.Terminal_rev42.Exceptions.InvalidUsernameException;
 import com.example.Terminal_rev42.Exceptions.PasswordAndConfirmedPasswordNotMatchException;
+import com.example.Terminal_rev42.Exceptions.UserAlreadyExistsException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
-@ControllerAdvice
+@ControllerAdvice(assignableTypes = UserController.class)
 public class UserControllerAdviser {
 
     private static final Logger logger = LoggerFactory.getLogger(BillController.class);
@@ -29,5 +32,15 @@ public class UserControllerAdviser {
         return ResponseEntity.badRequest().body(Map.of("message", "Check entered data.","password", exception.getMessage()));
     }
 
+    @ExceptionHandler(InvalidUsernameException.class)
+    private ResponseEntity<Map<String, String>> handleInvalidUsernameException(InvalidUsernameException exception, HttpServletRequest request){
+        logger.error("Exception InvalidUsername is thrown for " + request.getSession().getId() + ".");
+        return ResponseEntity.badRequest().body(Map.of("message", "Check input data.","username", exception.getMessage()));
+    }
 
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    private ResponseEntity<Map<String, String>> handleUserAlreadyExistsException(UserAlreadyExistsException exception, HttpServletRequest request){
+        logger.error("Exception UserAlreadyExists is thrown for " + request.getSession().getId() + ".");
+        return ResponseEntity.badRequest().body(Map.of("username", "Username: " + exception.getUsername() + " is already used.", "message", exception.getMessage()));
+    }
 }
